4fc812dcd13f088584e9539512b0a413fd174bf8
Seiji_Mitsuo <canalotakurafael2003@gmail.com>
2026-03-11 00:07:28 -0300

    Fix: botão tema escuro funcionando

---
+++ js/app.js
@@ -0,0 +1,10 @@
+function btnClaro() {
+    let claro = document.body.classList.add("temaClaro").value || [];
+
+    console.log(claro);
+}
+function btnEscuro(){
+    let escuro = document.body.classList.add("temaEscuro").value || [];
+    
+    console.log(escuro);
+}
