ad77422fc280506494795c3f9612856409b0bacd
Seiji_Mitsuo <canalotakurafael2003@gmail.com>
2026-03-12 23:30:56 -0300

    patch: modificado botão e adicionado icone

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
-const alternar = document.getElementById("btn1");
+const icone = document.getElementById("iconeTema");
 
 function mudar() {
-    document.body.classList.toggle("dark").value;
+
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")){
+        icone.classList.remove("bi-moon-fill");
+        icone.classList.add("bi-sun");
+    } else {
+        icone.classList.remove("bi-sun");
+        icone.classList.add("bi-moon-fill");
+    }
 }
 console.log(alternar)
